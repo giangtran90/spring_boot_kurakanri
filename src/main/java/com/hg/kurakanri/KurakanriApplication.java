5e294128469355d09ecf93800f30fd6ebb68d27e
ADMIN <giang08t3@gmail.com>
2023-09-24 11:15:36 +0900

    init project [ Quản lý kho ]

---
+++ src/main/java/com/hg/kurakanri/KurakanriApplication.java
@@ -0,0 +1,13 @@
+package com.hg.kurakanri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurakanriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KurakanriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hg/kurakanri/KurakanriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hg.kurakanri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KurakanriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
